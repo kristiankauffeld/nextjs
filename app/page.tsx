f9c4489f45790ebd7745bbf20dc48eed1c18665b
Kristian K. Andreasen <kristian.kauffeld@gmail.com>
2023-10-05 22:08:31 +0200

    uploadthing

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
-import Image from 'next/image';
+'use client';
+
+// You need to import our styles for the button to look right. Best to import in the root /layout.tsx but this is fine
+import '@uploadthing/react/styles.css';
+
+import { UploadButton } from './utils/uploadthing';
+import { Toaster } from '@/components/ui/toaster';
 
 export default function Home() {
   return (
-    <main>
-      <h1>hello world</h1>
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <UploadButton
+        endpoint='audioUploader'
+        onClientUploadComplete={(res) => {
+          // Do something with the response
+          console.log('Files: ', res);
+          alert('Upload Completed');
+        }}
+        onUploadError={(error: Error) => {
+          // Do something with the error.
+          alert(`ERROR! ${error.message}`);
+        }}
+      />
     </main>
   );
 }
